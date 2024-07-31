a8aa7a16e241c81dbb8ef4115df50ff07de8bc17
alexandre-girard <alexandre.girard@maif.fr>
2024-07-31 15:34:23 +0200

    ✨ use react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css'
 
-import characters from './data/characters.json'
-import { CharactersList } from './components/CharactersList'
-import { NumberOfCharacters } from './components/NumberOfCharacters'
-import AboutPage from './pages/AboutPage'
-import ContactPage from './pages/ContactPage'
-import CharactersPage from './pages/CharactersPage'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './routes'
+
+// Create a router that uses the client side history strategy for
+const router = createBrowserRouter(routes)
 
 function App() {
   return (
-    <>
-      {/* <h1>Marvel Characters</h1>
-      <CharactersList characters={characters} />
-      <br />
-      <NumberOfCharacters characters={characters} /> */}
-      {/* <AboutPage /> */}
-      {/* <ContactPage /> */}
-      <CharactersPage />
-    </>
+    <RouterProvider router={router} />
   )
 }
 

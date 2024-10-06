082f7e683a62c1bb1840ca9c95c7c236a835c17e
alexandre-girard <alexandre.girard@maif.fr>
2024-10-06 18:08:10 +0200

    ✨ Add character detail page

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
       {characters.map((character) => (
-        <li key={character.id}>{character.name}</li>
+        <li key={character.id}>
+          <Link to={`/characters/${character.id}`}>
+            {character.name}
+          </Link>
+        </li>
       ))}
     </ul>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,7 @@
+import { getCharacterById, getCharacters } from "./api/characters-api";
 import Layout from "./Layout";
 import AboutPage from "./pages/AboutPage";
+import CharacterDetailPage from "./pages/CharacterDetailPage";
 import CharactersPage from "./pages/CharactersPage";
 import ContactPage from "./pages/ContactPage";
 
@@ -8,7 +10,16 @@ const routes = [
         path: "/",
         element: <Layout />,
         children: [
-            { path: "/", element: <CharactersPage /> },
+            { 
+                path: "/", 
+                element: <CharactersPage />,
+                loader : () => getCharacters(),
+            },
+            {
+                path: "/characters/:id",
+                element: <CharacterDetailPage />,
+                loader: ({ params }) => getCharacterById(params.id),
+            },
             { path: "/about", element: <AboutPage /> },
             { path: "/contact", element: <ContactPage /> },
         ],

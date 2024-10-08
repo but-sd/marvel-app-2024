b70c9626ae2f14120c589fc65a9eab43d2f67b23
alexandre-girard <alexandre.girard@maif.fr>
2024-10-08 18:04:28 +0200

    bidon

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
     return (
         <>
             <header>
-                <h1>Marvel App</h1>
+                <h1>Marvel App ddd</h1>
                 <nav>
                     <NavLink to="/">Home</NavLink> - <NavLink to="/about">About</NavLink> - <NavLink to="/contact">Contact</NavLink>
                 </nav>
